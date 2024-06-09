e43c866d2927f15e76bcd9fc9d4764db9e8261da
William Mishra-Manning <manning.w27@gmail.com>
2024-06-09 11:07:48 -0400

    Optimize some UBO data handling

--- src/Renderer/world_viewport.jl
+++ src/Renderer/world_viewport.jl
@@ -10,6 +10,7 @@ mutable struct WorldViewport
     resolution::v2i
 
     ubo_read_data::FrameBufferReadData
+    ubo_write_data::FrameBufferWriteData
     ubo_read::Buffer
     ubo_write::Buffer
 
@@ -42,7 +43,7 @@ mutable struct WorldViewport
             foreground_depth,
             foreground_target, background_target,
             resolution,
-            ubo_read_data,
+            ubo_read_data, ubo_write_data,
             GL.Buffer(false, ubo_read_data),
             GL.Buffer(true, ubo_write_data)
         )
@@ -88,24 +89,22 @@ function render_to_framebuffer(callback_draw_world,
             GL.set_uniform_block(viewport.ubo_write, UBO_INDEX_FRAMEBUFFER_WRITE_DATA)
 
             # Draw foreground:
-            #TOOD: Reuse the FrameBufferWriteData because it allocates on the heap
-            GL.set_buffer_data(viewport.ubo_write, FrameBufferWriteData(
-                GL.get_ogl_handle(GL.get_view(assets.blank_depth_tex)),
-                true
-            ))
+            viewport.ubo_write_data.foreground_mode = true
+            viewport.ubo_write_data.tex_foreground_depth = GL.get_ogl_handle(GL.get_view(assets.blank_depth_tex))
+            GL.set_buffer_data(viewport.ubo_write, viewport.ubo_write_data)
             GL.target_clear(viewport.foreground_target,
                             vRGBAu(Val(~zero(UInt32))),
                             1)
             GL.target_clear(viewport.foreground_target, Float32(1))
             GL.target_activate(viewport.foreground_target)
+            GL.view_activate(assets.blank_depth_tex)
             callback_draw_world(RenderPass.foreground)
+            GL.view_deactivate(assets.blank_depth_tex)
 
             # Draw background:
-            #TOOD: Reuse the FrameBuffeRWriteData because it allocates on the heap
-            GL.set_buffer_data(viewport.ubo_write, FrameBufferWriteData(
-                GL.get_ogl_handle(GL.get_view(viewport.foreground_depth)),
-                false
-            ))
+            viewport.ubo_write_data.foreground_mode = false
+            viewport.ubo_write_data.tex_foreground_depth = GL.get_ogl_handle(GL.get_view(viewport.foreground_depth))
+            GL.set_buffer_data(viewport.ubo_write, viewport.ubo_write_data)
             GL.target_clear(viewport.background_target,
                             vRGBAu(Val(~zero(UInt32))),
                             1)
